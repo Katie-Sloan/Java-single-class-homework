fa13098d97490bd3be5843cdcca718ad84f05243
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-05 21:07:27 +0000

    Modified tests to check toner reduces, extension complete

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,14 +1,17 @@
 public class Printer {
     private int sheets;
+    public int tonerVolume;
 
-    public Printer(int sheets) {
+    public Printer(int sheets, int tonerVolume) {
         this.sheets = sheets;
+        this.tonerVolume = tonerVolume;
     }
 
     public int print(int pages, int copies) {
         if (this.sheets - (pages * copies) < 0) {
             return this.sheets;
         }
+        this.tonerVolume -= (pages * copies);
         return this.sheets - (pages * copies);
     };
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,21 +8,23 @@ public class PrinterTest {
     Printer printer;
 
     @Before
-    public void setUp() { printer = new Printer(500); }
+    public void setUp() { printer = new Printer(500, 1000); }
 
     @Test
     public void successfulPrintSurplusSheets(){
         assertEquals(100, printer.print(100, 4));
+        assertEquals(600, printer.tonerVolume);
     }
 
     @Test
     public void successfulPrintZeroSheets(){
         assertEquals(0, printer.print(10, 50));
+        assertEquals(500, printer.tonerVolume);
     }
 
     @Test
     public void unsuccessfulPrint(){
         assertEquals(500, printer.print(51, 10));
+        assertEquals(1000, printer.tonerVolume);
     }
-
 }

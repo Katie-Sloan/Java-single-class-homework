ba951ef13daeb2242f070bae82a7e7a74b8607c6
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-05 20:04:01 +0000

    drink test passing

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void setUp() { waterBottle = new WaterBottle(100); }
+
+    @Test
+    public void drink(){
+        assertEquals(90, waterBottle.drink());
+    }
+}
